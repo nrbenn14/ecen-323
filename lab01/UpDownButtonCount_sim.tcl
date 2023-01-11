d694d1d732be5b42c85f0bd22a249f50f5ae4c3e
nrbenn14 <nrbenn14@byu.edu>
2023-01-10 17:58:48 -0700

    Edits on tcl and xdc files

--- lab01/UpDownButtonCount_sim.tcl
+++ lab01/UpDownButtonCount_sim.tcl
@@ -1,9 +1,9 @@
 ##########################################################################
 #
-# ButtonCount_sim.tcl
-# Author: Mike Wirthlin
+# UpDownButtonCount_sim.tcl
+# Author: Nathan
 # Class: ECEN 323
-# Date: 12/10/2020
+# Date: 1/10/2023
 #
 # This .tcl script will apply the input stimulus to the circuit
 # as shown in the waveform in the lab wiki.
@@ -47,9 +47,61 @@ run 10 ns
 add_force btnu 0
 run 30 ns
 
-# set inc (btnu)=1 for two clock cycles
+# set inc (btnu)=1 for one clock cycles
 add_force btnu 1
+run 10 ns
+
+# set inc (btnu)=0 for one clock cycles
+add_force btnu 0
+run 10 ns
+
+# set inc (btnu)=1 for one clock cycles
+add_force btnu 1
+run 10 ns
+
+# set inc (btnu)=0 for one clock cycles
+add_force btnu 0
+run 10 ns
+
+# set decrement (btnd)=1 for two clock cycles
+add_force btnd 1
 run 20 ns
+
+# set decrement (btnd)=0 for three clock cycles
+add_force btnd 0
+run 30 ns
+
+# set decrement (btnd)=1 for four clock cycles
+add_force btnd 1
+run 40 ns
+
+# set decrement (btnd)=0 for one clock cycles
+add_force btnd 0
+run 10 ns
+
+# set the switches (sw)=4 for two clock cycles
+add_force sw 0000000000000100
+run 20 ns
+
+# set switch increment (btnr)=1 for three clock cycles
+add_force btnr 1
+run 30 ns
+
+# set switch increment (btnr)=0 for three clock cycles
+add_force btnr 0
+run 30 ns
+
+# set the switches (sw)=2 for two clock cycles
+add_force sw 0000000000000010
+run 20 ns
+
+# set switch decrement (btnl)=1 for three clock cycles
+add_force btnl 1
+run 30 ns
+
+# set switch decrement (btnl)=0 for three clock cycles
+add_force btnl 0
+run 30 ns
 
 # Issue a reset (btnc) for one clock cycle
 add_force btnc 1
