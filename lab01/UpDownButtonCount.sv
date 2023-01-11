16a1acf58577399a91ed851af8e96d15c1b9edd0
nrbenn14 <nrbenn14@byu.edu>
2023-01-10 17:36:53 -0700

    Adding all the UpDownButton files

--- lab01/UpDownButtonCount.sv
+++ lab01/UpDownButtonCount.sv
@@ -1,55 +1,54 @@
-// This timescale statement indicates that each time tick of the simulator
-// is 1 nanosecond and the simulator has a precision of 1 picosecond. This 
-// is used for simulation and all of your SystemVerilog files should have 
-// this statement at the top. 
 `timescale 1 ns / 1 ps 
 
 /***************************************************************************
 * 
-* File: ButtonCount.sv
+* File: UpDownButtonCount.sv
 *
-* Author: Professor Mike Wirthlin
-* Class: ECEN 323, Winter Semester 2020
-* Date: 12/10/2020
+* Author: Nathan Bennion
+* Class: ECEN 323, Winter Semester 2023
+* Date: 1/10/2023
 *
-* Module: ButtonCount
+* Module: UpDownButtonCount
 *
 * Description:
-*    This module includes a state machine that will provide a one cycle
-*    signal every time the top button (btnu) is pressed (this is sometimes
-*    called a 'single-shot' filter of the button signal). This signal
-*    is used to increment a counter that is displayed on the LEDs. The
-*    center button (btnc) is used as an asynchronous reset.
-*
-*    This module is used to help students review their RTL design skills and
-*    get the design tools working.  
+*    This module includes logic to do specific outputs to the LEDs based on
+*    the button inputs. An up press will increment by one, a down press will
+*    decrement by 1, a left press will decrement by the binary amount based
+*    on the switches, and a right press will increment by the switches.
 *
 ****************************************************************************/
 
-module ButtonCount(clk, btnc, btnu, led);
+module UpDownButtonCount(clk, btnc, btnu, btnd, btnl, btnr, led, sw);
 
-	input wire logic clk, btnc, btnu;
+	input wire logic clk, btnc, btnu, btnd, btnl, btnr;
+	input wire logic [15:0] sw;
 	output logic [15:0] led;
 	
 	// The internal 16-bit count signal. 
 	logic [15:0] count_i;
 	// The increment counter output from the one shot module
-	logic inc_count;
+	logic inc_count_up, inc_count_down, inc_count_left, inc_count_right;
 	// reset signal
 	logic rst;
 	// increment signals (synchronized version of btnu)
-	logic btnu_d, btnu_dd, inc;
+	logic btnu_d, btnu_dd;
+	logic btnd_d, btnd_dd;
+	logic btnl_d, btnl_dd;
+	logic btnr_d, btnr_dd;
+	
+	// outputted incremented signals (to the OneShot module)
+	logic inc_up, inc_down, inc_left, inc_right;
 
 	// Assign the 'rst' signal to button c
 	assign rst = btnc;
 
 	// The following always block creates a "synchronizer" for the 'btnu' input.
 	// A synchronizer synchronizes the asynchronous 'btnu' input to the global
-	// clock (when you press a button you are not synchronous with anything!).
+	// clock.
 	// This particular synchronizer is just two flip-flop in series: 'btnu_d'
 	// is the first flip-flop of the synchronizer and 'btnu_dd' is the second
-	// flip-flop of the synchronizer. You should always have a synchronizer on
-	// ant button input if they are used in a sequential circuit.
+	// flip-flop of the synchronizer.
+	
 	always_ff@(posedge clk)
 		if (rst) begin
 			btnu_d <= 0;
@@ -59,24 +58,76 @@ module ButtonCount(clk, btnc, btnu, led);
 			btnu_d <= btnu;
 			btnu_dd <= btnu_d;
 		end
-	assign inc = btnu_dd;
+	assign inc_up = btnu_dd;
+	
+	// Symchronizers for each of the other buttons
+	
+	// btnd input
+	always_ff@(posedge clk)
+		if (rst) begin
+			btnd_d <= 0;
+			btnd_dd <= 0;
+		end
+		else begin
+			btnd_d <= btnd;
+			btnd_dd <= btnd_d;
+		end
+	assign inc_down = btnd_dd;
+	
+	// btnl input
+	always_ff@(posedge clk)
+		if (rst) begin
+			btnl_d <= 0;
+			btnl_dd <= 0;
+		end
+		else begin
+			btnl_d <= btnl;
+			btnl_dd <= btnl_d;
+		end
+	assign inc_left = btnl_dd;
+	
+	// btnr input
+	always_ff@(posedge clk)
+		if (rst) begin
+			btnr_d <= 0;
+			btnr_dd <= 0;
+		end
+		else begin
+			btnr_d <= btnr;
+			btnr_dd <= btnr_d;
+		end
+	assign inc_right = btnr_dd;
+	
+	
 
-	// Instance the OneShot module
-	OneShot os (.clk(clk), .rst(rst), .in(inc), .os(inc_count));
+	// Instances of the OneShot module for each of the directional buttons
+	OneShot osu (.clk(clk), .rst(rst), .in(inc_up), .os(inc_count_up));
+	OneShot osd (.clk(clk), .rst(rst), .in(inc_down), .os(inc_count_down));
+	OneShot osl (.clk(clk), .rst(rst), .in(inc_left), .os(inc_count_left));
+	OneShot osr (.clk(clk), .rst(rst), .in(inc_right), .os(inc_count_right));	
+	
 
-	// 16-bit Counter. Increments once each time button is pressed. 
-	//
-	// This is an exmaple of a 'sequential' statement that will synthesize flip-flops
-	// as well as the logic for incrementing the count value.
-	//
-	//  CODING STANDARD: Every "segment/block" of your RTL code must have at least
-	//  one line of white space between it and the previous and following block. Also,
-	//  ALL always blocks must have a coment.
+	// 16-bit Counter. Increments or decrements based on the button is pressed. 
+
 	always_ff@(posedge clk)
 		if (rst)
 			count_i <= 0;
-		else if (inc_count)
+			
+        // Logic for incrementing
+		else if (inc_count_up)
 			count_i <= count_i + 1;
+			
+        // Logic for decrementing
+        else if (inc_count_down)
+            count_i <= count_i - 1;
+            
+        // Logic for decrementing based on the switch
+        else if (inc_count_left)
+            count_i <= count_i - sw;
+            
+        // Logic for incrementing based on the switch
+        else if (inc_count_right)
+            count_i <= count_i + sw;
 	
 	// Assign the 'led' output the value of the internal count_i signal.
 	assign led = count_i;
